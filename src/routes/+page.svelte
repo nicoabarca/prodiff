0d9ca381a34ca6e7bc623a72c33d04d621d2e880
Nicolás Abarca <nicoabarca@uc.cl>
2026-07-20 19:55:56 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  let numbers = $state([1, 2, 3]);
+  let total = $derived.by(() => {
+    let total = 0;
+    for (const n of numbers) {
+      total += n;
+    }
+    return total;
+  });
+</script>
+
+<Button onclick={() => numbers.push(numbers.length + 1)}>
+  {numbers.join(" + ")} = {total}
+</Button>
